c847fa9a729266e4c9252f5a629eb045dbf4903d
unknown <you@example.com>
2020-04-04 00:40:56 +0200

    Lead seaction finished

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,7 +1,20 @@
-$(document).ready(function(){
-    
-    
-    
-    
-})
+$(document).ready(function () {
+
+    //Lead section - check header height
+    function checkHeaderHeight() {
+        //take Height with padding
+        let headerHeight = $('header').innerHeight();
+        //set header height as main's margin top 
+        $('main').css('margin-top', headerHeight);
+    }
+
+    //call function
+    checkHeaderHeight();
+    $(window).resize(function () {
+        checkHeaderHeight();
+    });
+
+
+
+});
 
