e8f24653fcc007fec38d784efe899e42f368651a
unknown <you@example.com>
2020-04-15 22:46:58 +0200

    404 finished

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -51,55 +51,58 @@ $(document).ready(function () {
     // form validating
     if ($('.contact-form').length > 0) {
 
-        $('.contact-form').validate({
+        //FORM VALIDATION
+        $(function () {
+            $(".contact-form").validate({
+                highlight: function (element) {
+                    $(element).addClass("is-invalid").removeClass("is-valid");
+                },
+                unhighlight: function (element) {
+                    $(element).removeClass('is-invalid').addClass('is-valid');
+                },
+                rules: {
+                    name: {
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    subject: {
+                        required: true
+                    },
+                    message: {
+                        required: true
+                    }
 
-            highlight: function (element) {
-                $(element).addClass('is-invalid').removeClass('is-valid');
-            },
-            unhighlight: function (element) {
-                $(element).addClass('is-valid').removeClass('is-invalid');
-            },
-            rules: {
-                name: {
-                    required: true
+
                 },
-                email: {
-                    required: true,
-                    email: true
+                messages: {
+
+                    name: {
+                        required: 'The Name* is required field.'
+                    },
+                    email: {
+                        required: 'The Email* is required field.',
+                        email: 'Please provide a valid Email address.'
+                    },
+                    subject: {
+                        required: 'The Subject* is required field.'
+                    },
+                    message: {
+                        required: 'The Message* is required field.'
+                    }
+
+
+
                 },
-                subject: {
-                    required: true,
-                },
-                message: {
-                    required: true
+                errorElement: 'p',
+                errorPlacement: function (error, element) {
+                    error.appendTo($(element).closest('.form-group').find('.invalid-feedback'));
                 }
-            },
-            messages: {
-                name: {
-                    required: 'The Name* field is required'
-                },
-                email: {
-                    required: 'The Email* field is required',
-                    email: 'Please provide a valid email address'
-                },
-                subject: {
-                    required: 'The Subject* field is required'
-                },
-                message: {
-                    required: 'The Message* field is required'
-                }
-            },
 
-            errorElement: 'p',
-            errorPlacement: function (error, element) {
-                error.appendTo(element.closest(".form-group").find(".error-msg"));
-            }
-
-
-
-
+            });
         });
-
     }
 
 
